74bd5a0e9ae6cf32d7ffecc340282ff601d05973
Ascanio Colonna di Paliano <a.colonna@gmail.com>
2025-10-16 23:16:30 -0400

    Progress

--- js/main.js
+++ js/main.js
@@ -3,12 +3,21 @@ var dragBubble;
 var timeTempChart;
 var dragBoundingBox;
 var dragBounds;
+var previewVerticalLine;
+var previewHorizontalLine;
+var horizontalSpan;
+var verticalSpan;
+var lastMovePosition = [0, 0];
 
 
 document.addEventListener('DOMContentLoaded', handlePageLoaded);
 
 function handlePageLoaded(event) {
     timeTempChart = document.getElementById('timeTempChart');
+    dragBounds = timeTempChart.getBoundingClientRect();
+    horizontalSpan = dragBounds.width / 12;
+    verticalSpan = dragBounds.height / 24;
+    drawTimeTempChart(timeTempChart);
     
     timeTempChart.addEventListener('mousedown', handleMouseDown);
     window.addEventListener('mouseup', handleMouseUp);
@@ -17,29 +26,50 @@ function handlePageLoaded(event) {
     window.addEventListener('touchend', handleMouseUp);
 }
 
-function handleMouseDown(event) {
-    console.log('start', event);
+function drawTimeTempChart(container) {
+    const fragment = new DocumentFragment();
+    var i, iLimit = 24, j, jLimit = 12, ySpan = dragBounds.height / iLimit, xSpan = dragBounds.width / jLimit;
+    for(i = 0; i <= iLimit; i++) {
+        if(i % 4 == 0) {
+            // add a line
+            const line = document.createElement('div');
+            line.className = 'chartLine';
+            line.style.top = `${ySpan * i}px`;
+            line.style.left = '0px;';
+            fragment.appendChild(line);
+        } else {
+            // add dots
+            for(j = 0; j <= jLimit; j++) {
+                const dot = document.createElement('div');
+                dot.className = 'chartDot';
+                dot.style.top = `${ySpan * i}px`;
+                dot.style.left = `${xSpan * j}px`;
+                fragment.appendChild(dot);
+            }
+        }
+    }
+    container.appendChild(fragment);
+}
 
+function handleMouseDown(event) {
     dragBoundingBox = event.target;
-    dragBounds = dragBoundingBox.getBoundingClientRect();
-    console.log('dragBounds', dragBounds);
 
     var x, y;
     switch(event.type) {
         case "mousedown":
-            x = event.clientX;
-            y = event.clientY;
+            x = lastMovePosition[0] = event.clientX;
+            y = lastMovePosition[1] = event.clientY;
             break;
         case "touchstart":
             console.log(event);
             const touch = event.touches[0];
-            x = touch.clientX;
-            y = touch.clientY;
+            x = lastMovePosition[0] = touch.clientX;
+            y = lastMovePosition[1] = touch.clientY;
             break;
         default:
             return;
     }
-    // TODO: change to discriminate between dragging an existing item or creating a new one
+
     const container = document.getElementsByTagName('body')[0];
     const fragment = new DocumentFragment();
     dragBubble = document.createElement('div');
@@ -48,6 +78,16 @@ function handleMouseDown(event) {
     dragBubble.style.left = `${x}px`;
     dragBubble.style.top = `${y}px`;
     fragment.appendChild(dragBubble);
+    previewVerticalLine = document.createElement('div');
+    previewVerticalLine.className = 'previewLine';
+    previewVerticalLine.style.height = '100%';
+    previewVerticalLine.style.left = `${Math.round((x-dragBounds.x)/horizontalSpan)*horizontalSpan}px`;
+    timeTempChart.appendChild(previewVerticalLine);
+    previewHorizontalLine = document.createElement('div');
+    previewHorizontalLine.className = 'previewLine';
+    previewHorizontalLine.style.width = '100%';
+    previewHorizontalLine.style.top = `${Math.round((y-dragBounds.y)/verticalSpan)*verticalSpan}px`;
+    timeTempChart.appendChild(previewHorizontalLine);
     container.appendChild(fragment);
     isDragging = true;
     timeTempChart.addEventListener('mousemove', handleDrag);
@@ -56,32 +96,61 @@ function handleMouseDown(event) {
 
 function handleMouseUp(event) {
     console.log('end');
-    // TODO: delete bubble
     if(dragBubble) {
         dragBubble.remove();
         dragBubble = null;
     }
+    if(previewVerticalLine) {
+        previewVerticalLine.remove();
+        previewVerticalLine = null;
+    }
+    if(previewHorizontalLine) {
+        previewHorizontalLine.remove();
+        previewHorizontalLine = null;
+    }
     isDragging = false;
     timeTempChart.removeEventListener('mousemove', handleDrag);
     timeTempChart.removeEventListener('touchmove', handleDrag);
+
+
+    var x = lastMovePosition[0] - dragBounds.x, y = lastMovePosition[1] - dragBounds.y, xPos, yPos;
+    
+    var xPos = Math.round(x / horizontalSpan);
+    var yPos = Math.round(y / verticalSpan);
+
+    const fragment = new DocumentFragment();
+    const bubble = document.createElement('div');
+    bubble.id = 'tempBubble';
+    bubble.innerHTML = xPos + 18;
+    bubble.style.left = `${xPos*horizontalSpan}px`;
+    bubble.style.top = `${yPos*verticalSpan}px`;
+    fragment.appendChild(bubble);
+    timeTempChart.appendChild(fragment);
+    console.log(bubble);
 }
 
 function handleDrag(event) {
-    var x, y;
+    var x, y, oX, oY;
     switch(event.type) {
         case "mousemove":
-            x = event.clientX;
-            y = event.clientY;
+            x = lastMovePosition[0] = event.clientX;
+            y = lastMovePosition[1] = event.clientY;
             break;
         case "touchmove":
             const touch = event.touches[0];
-            x = touch.clientX;
-            y = touch.clientY;
+            x = lastMovePosition[0] = touch.clientX;
+            y = lastMovePosition[1] = touch.clientY;
             break;
         default:
             return;
     }
-    console.log('drag');
-    dragBubble.style.left = `${x}px`;
-    dragBubble.style.top = `${y}px`;
+    oX = x - dragBounds.x;
+    oY = y - dragBounds.y;
+    if(oX > 0 && oX < dragBounds.width && oY > 0 && oY < dragBounds.height) {
+        dragBubble.style.left = `${x}px`;
+        dragBubble.style.top = `${y}px`;
+
+        previewVerticalLine.style.left = `${Math.round(oX/horizontalSpan)*horizontalSpan}px`;
+        previewHorizontalLine.style.top = `${Math.round(oY/verticalSpan)*verticalSpan}px`;
+    }
 }
