8c175a4e9d45081ebe81786513d891a7523e778e
Ascanio Colonna di Paliano <a.colonna@gmail.com>
2025-10-16 21:39:49 -0400

    Progress

--- js/main.js
+++ js/main.js
@@ -8,7 +8,6 @@ var dragBounds;
 document.addEventListener('DOMContentLoaded', handlePageLoaded);
 
 function handlePageLoaded(event) {
-    /* Info.getLocalIP(handleLocalIPFound); */
     timeTempChart = document.getElementById('timeTempChart');
     
     timeTempChart.addEventListener('mousedown', handleMouseDown);
@@ -17,24 +16,6 @@ function handlePageLoaded(event) {
     timeTempChart.addEventListener('touchstart', handleMouseDown);
     window.addEventListener('touchend', handleMouseUp);
 }
-
-/* function handleLocalIPFound(myIP) {
-    // TODO: populate myIP pill on screen
-    const container = document.getElementsByTagName('body')[0];
-    const fragment = new DocumentFragment();
-    const label = document.createElement('span');
-    label.innerHTML = `IP: ${myIP}`;
-    const button = document.createElement('button');
-    button.type = 'button';
-    button.innerHTML = 'X';
-    button.onclick = (e) => { e.target.parentElement.remove(); };
-    const pill = document.createElement('div');
-    pill.className = 'ipPill';
-    pill.appendChild(label);
-    pill.appendChild(button);
-    fragment.appendChild(pill);
-    container.appendChild(fragment);
-} */
 
 function handleMouseDown(event) {
     console.log('start', event);
