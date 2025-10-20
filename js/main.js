a8a08110736d4cdeba64000d0e73966f38953830
Ascanio Colonna di Paliano <a.colonna@gmail.com>
2025-10-20 13:28:25 -0400

    Fix

--- js/main.js
+++ js/main.js
@@ -16,27 +16,17 @@ var savedBubbles = [];
 document.addEventListener('DOMContentLoaded', handlePageLoaded);
 
 window.oncontextmenu = function (event) {
-    // eslint-disable-next-line no-console
-    console.log(event); // prints [object PointerEvent]
-  
     const pointerEvent = event;
-    // eslint-disable-next-line no-console
-    console.log(`window.oncontextmenu: ${pointerEvent.pointerType}`);
   
     if (pointerEvent.pointerType === 'touch') {
-      // context menu was triggerd by long press
       return false;
     }
   
-    // just to show that pointerEvent.pointerType has another value 'mouse' aka right click
     if (pointerEvent.pointerType === 'mouse') {
-      // context menu was triggered by right click
       return true;
     }
-  
-    // returning true will show a context menu for other cases
     return true;
-  };
+}
 
 function handlePageLoaded(event) {
     timeTempChart = document.getElementById('timeTempChart');
