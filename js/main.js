84b2bfefcee8631bc06530561410529ada77069c
Ascanio Colonna di Paliano <a.colonna@gmail.com>
2025-10-20 13:04:29 -0400

    Cleanup

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ var isDragging = false;
 var dragBubble;
 var upperBubble;
 var timeTempChart;
+var chartLabels;
 var dragBoundingBox;
 var dragBounds;
 var previewVerticalLine;
@@ -9,12 +10,37 @@ var previewHorizontalLine;
 var horizontalSpan;
 var verticalSpan;
 var lastMovePosition = [0, 0];
+var savedBubbles = [];
 
 
 document.addEventListener('DOMContentLoaded', handlePageLoaded);
 
+window.oncontextmenu = function (event) {
+    // eslint-disable-next-line no-console
+    console.log(event); // prints [object PointerEvent]
+  
+    const pointerEvent = event;
+    // eslint-disable-next-line no-console
+    console.log(`window.oncontextmenu: ${pointerEvent.pointerType}`);
+  
+    if (pointerEvent.pointerType === 'touch') {
+      // context menu was triggerd by long press
+      return false;
+    }
+  
+    // just to show that pointerEvent.pointerType has another value 'mouse' aka right click
+    if (pointerEvent.pointerType === 'mouse') {
+      // context menu was triggered by right click
+      return true;
+    }
+  
+    // returning true will show a context menu for other cases
+    return true;
+  };
+
 function handlePageLoaded(event) {
     timeTempChart = document.getElementById('timeTempChart');
+    chartLabels = document.getElementById('chartLabels');
     dragBounds = timeTempChart.getBoundingClientRect();
     horizontalSpan = dragBounds.width / 12;
     verticalSpan = dragBounds.height / 24;
@@ -71,6 +97,11 @@ function handleMouseDown(event) {
             return;
     }
 
+    var oX = x - dragBounds.x;
+    var oY = y - dragBounds.y;
+    var xPos = Math.round(oX / horizontalSpan);
+    var yPos = Math.round(oY / verticalSpan);
+
     const container = document.getElementsByTagName('body')[0];
     const fragment = new DocumentFragment();
     dragBubble = document.createElement('div');
@@ -80,27 +111,34 @@ function handleMouseDown(event) {
     fragment.appendChild(dragBubble);
     upperBubble = document.createElement('div');
     upperBubble.id = 'upperBubble';
-    upperBubble.innerText = Math.round((x - dragBounds.x) / horizontalSpan) + 18;
+    upperBubble.innerText = xPos + 18;
     dragBubble.appendChild(upperBubble);
 
     previewVerticalLine = document.createElement('div');
     previewVerticalLine.className = 'previewLine';
     previewVerticalLine.style.height = '100%';
-    previewVerticalLine.style.left = `${Math.round((x-dragBounds.x)/horizontalSpan)*horizontalSpan}px`;
+    previewVerticalLine.style.left = `${xPos*horizontalSpan}px`;
     timeTempChart.appendChild(previewVerticalLine);
     previewHorizontalLine = document.createElement('div');
     previewHorizontalLine.className = 'previewLine';
     previewHorizontalLine.style.width = '100%';
-    previewHorizontalLine.style.top = `${Math.round((y-dragBounds.y)/verticalSpan)*verticalSpan}px`;
+    previewHorizontalLine.style.top = `${yPos*verticalSpan}px`;
     timeTempChart.appendChild(previewHorizontalLine);
     container.appendChild(fragment);
     isDragging = true;
     timeTempChart.addEventListener('mousemove', handleDrag);
     timeTempChart.addEventListener('touchmove', handleDrag);
+
+    var existingBubble = getBubble(xPos, yPos);
+    if(existingBubble != null) {
+        const bubble = existingBubble.bubble;
+        console.log(bubble);
+        bubble.style.display = 'none';
+        savedBubbles.splice(existingBubble.index, 1);
+    }
 }
 
 function handleMouseUp(event) {
-    console.log('end');
     if(dragBubble) {
         dragBubble.remove();
         dragBubble = null;
@@ -118,20 +156,23 @@ function handleMouseUp(event) {
     timeTempChart.removeEventListener('touchmove', handleDrag);
 
 
-    var x = lastMovePosition[0] - dragBounds.x, y = lastMovePosition[1] - dragBounds.y, xPos, yPos;
-    
+    var x = lastMovePosition[0] - dragBounds.x, y = lastMovePosition[1] - dragBounds.y;
     var xPos = Math.round(x / horizontalSpan);
     var yPos = Math.round(y / verticalSpan);
+    
+    if(!bubbleAtSameTime(yPos)) {
+        const fragment = new DocumentFragment();
+        const bubble = document.createElement('div');
+        bubble.className = 'tempBubble';
+        bubble.innerHTML = xPos + 18;
+        bubble.style.left = `${xPos*horizontalSpan}px`;
+        bubble.style.top = `${yPos*verticalSpan}px`;
+        fragment.appendChild(bubble);
+        timeTempChart.appendChild(fragment);
+        savedBubbles.push([xPos, yPos, bubble]);
+    }
 
-    const fragment = new DocumentFragment();
-    const bubble = document.createElement('div');
-    bubble.id = 'tempBubble';
-    bubble.innerHTML = xPos + 18;
-    bubble.style.left = `${xPos*horizontalSpan}px`;
-    bubble.style.top = `${yPos*verticalSpan}px`;
-    fragment.appendChild(bubble);
-    timeTempChart.appendChild(fragment);
-    console.log(bubble);
+    cleanUpChart();
 }
 
 function handleDrag(event) {
@@ -151,6 +192,8 @@ function handleDrag(event) {
     }
     oX = x - dragBounds.x;
     oY = y - dragBounds.y;
+    var xPos = Math.round(oX / horizontalSpan);
+    var yPos = Math.round(oY / verticalSpan);
     if(oX > 0 && oX < dragBounds.width && oY > 0 && oY < dragBounds.height) {
         lastMovePosition[0] = x;
         lastMovePosition[1] = y;
@@ -158,7 +201,54 @@ function handleDrag(event) {
         dragBubble.style.top = `${y}px`;
         const tempValue = Math.round(oX / horizontalSpan) + 18;
         upperBubble.innerText = tempValue;
-        previewVerticalLine.style.left = `${Math.round(oX/horizontalSpan)*horizontalSpan}px`;
-        previewHorizontalLine.style.top = `${Math.round(oY/verticalSpan)*verticalSpan}px`;
+        previewVerticalLine.style.left = `${xPos*horizontalSpan}px`;
+        previewHorizontalLine.style.top = `${yPos*verticalSpan}px`;
+        if(bubbleAtSameTime(yPos)) {
+            previewHorizontalLine.style.display = 'none';
+            previewVerticalLine.style.display = 'none';
+        } else {
+            previewHorizontalLine.style.display = 'block';
+            previewVerticalLine.style.display = 'block';
+        }
     }
+}
+
+function getBubble(xPos, yPos) {
+    var result = null;
+    var i, limit = savedBubbles.length;
+    for(i=0; i<limit; i++) {
+        const bubble = savedBubbles[i];
+        if(bubble[0] == xPos && bubble[1] == yPos) {
+            result = {index: i, bubble: bubble[2]};
+            break;
+        }
+    }
+    return result;
+}
+
+function bubbleAtSameTime(yPos) {
+    var result = false;
+    var i, limit = savedBubbles.length;
+    for(i=0; i<limit; i++) {
+        const bubble = savedBubbles[i];
+        if(bubble[1] == yPos) {
+            result = true;
+            break;
+        }
+    }
+    return result;
+}
+
+function cleanUpChart() {
+    console.log('cleanUpChart');
+    const allBubbles = timeTempChart.querySelectorAll('div.tempBubble');
+    console.log('found bubbles', allBubbles.length);
+    allBubbles.forEach(child => {
+        const computedStyle = window.getComputedStyle(child);
+        if(computedStyle.display == 'none') {
+            console.log('removing child');
+            child.remove();
+            child = null;
+        }
+    });
 }
